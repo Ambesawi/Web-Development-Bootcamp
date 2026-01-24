cbc41078d3156e39ebcac49eb38bf33499a3f5fd
Ambesawi <ambesawi77@gmail.com>
2026-01-23 23:58:40 -0700

    function_part_3.js

---
+++ section_14/function_part_3.js
@@ -0,0 +1,13 @@
+function getMilk(money) {
+    console.log("leaveHouse");
+    console.log("moveRight");
+    
+    var numberOfBottles = Math.floor(money / 1.5);
+    console.log("buy " + numberOfBottles + "bottles of milk");
+
+    console.log("moveRight");
+    console.log("enterHouse");
+
+}
+
+getMilk(5);
