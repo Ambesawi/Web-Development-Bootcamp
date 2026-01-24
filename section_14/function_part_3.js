f06ad517f9e33380cd5033e620d917c6a59e4515
Ambesawi <ambesawi77@gmail.com>
2026-01-24 10:01:03 -0700

    function_part_3.js

--- section_14/function_part_3.js
+++ section_14/function_part_3.js
@@ -1,13 +1,21 @@
 function getMilk(money) {
     console.log("leaveHouse");
     console.log("moveRight");
-    
-    var numberOfBottles = Math.floor(money / 1.5);
-    console.log("buy " + numberOfBottles + "bottles of milk");
+
+    console.log("buy " + calcBottles(money, 1.5) + " bottles of milk");
+    // console.log("buy " + numberOfBottles + "bottles of milk");
 
     console.log("moveRight");
     console.log("enterHouse");
 
+    return money % 1.5; //Reminder of this division
+
 }
 
-getMilk(5);
+function calcBottles(startingMony, costPerBottles) {
+ 
+    var numberOfBottles = Math.floor(startingMony / costPerBottles);
+    return numberOfBottles;
+}
+
+getMilk(4);
