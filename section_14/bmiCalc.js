1348c60cf8dbf6b061fe04d2e9e6bc3306727992
Ambesawi <ambesawi77@gmail.com>
2026-01-26 00:12:35 -0700

    bmiCalc.js

--- section_14/bmiCalc.js
+++ section_14/bmiCalc.js
@@ -1,9 +1,19 @@
 
-
-function calcBMI(hight, wight){
-    var bmi = hight /(wight*wight);
+function bmiCalculator(weight, height){
+    var bmi = weight /(height*height);
     return bmi;
 }
 
-var result = Math.floor(calcBMI(65, 1.8));
-console.log("your BMI is " + result + " in tis case");
+var result = Math.floor(bmiCalculator(50, 1.8));
+
+if(result < 18.5){
+    console.log("Your BMI is " + result + ", so you are underweight.");
+}
+
+if(result >= 18.5 && result <= 24.9){
+    console.log("Your BMI is " + result + ", so you have a normal weight.");
+}
+
+if(result > 24.9){
+    console.log("Your BMI is " + result + ", so you are overweight.");
+}
