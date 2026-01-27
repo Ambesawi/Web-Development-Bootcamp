dedaa4613e0c90278322672ad0987de2d53e24dc
Ambesawi <ambesawi77@gmail.com>
2026-01-27 02:23:28 -0700

    gustList.js

---
+++ section_14/gustList.js
@@ -0,0 +1,8 @@
+var gustList = ["Anjela", "Aman", "sami", "danny" ];
+    var gustName = prompt("pmput your gust name");
+    if (gustList.includes(gustName)){
+        alert("your allowd to come in");
+    } else{
+        alert("you are not invited");
+    }
+
