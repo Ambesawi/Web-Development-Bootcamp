97d781a961ecdfb230bbe59e0d6f277c25f757e7
Ambesawi <ambesawi77@gmail.com>
2026-02-09 01:54:06 -0700

    calculator.js

---
+++ section_18/Drum_Kit_Starting_Files/Drum_Kit_Starting_Files/calculator.js
@@ -0,0 +1,21 @@
+
+
+function add(num1, num2){
+return num1 + num2;
+}
+
+function subtract(num1, num2){
+    return num1 - num2;
+}
+
+function multiply(num1, num2){
+    return num1 * num2;
+}
+
+function division(num1, num2){
+    return num1 /num2;
+}
+
+function calculator(num1, num2, oprator){
+    return oprator(num1, num2);
+}
