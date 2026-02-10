e791d113f8d2843f064637ff43e02d0aa87db72a
Ambesawi <ambesawi77@gmail.com>
2026-02-09 22:18:05 -0700

    object.js

--- section_18/Drum_Kit_Starting_Files/Drum_Kit_Starting_Files/object.js
+++ section_18/Drum_Kit_Starting_Files/Drum_Kit_Starting_Files/object.js
@@ -1,13 +1,22 @@
-
+// Object literal example
 // creating obeject calld houskeeper
 
-var houskeeper =
-
+ /** var houseKeeper1 = {
+    yearsOfExperience: 12,
+    name: "Jane",
+    cleaningRepetoire: ["bathroom", "lobby", "bedromm"]
+}
+ **/
 
 // Constructor Function for the object. 
 // Its like creating a fuctory for the object house keeper
+// In Constructor function the  first alphabet of the name has to be in capital leter 
+function HouseKeeper1(yearsOfExperience, name, cleaningRepetoire){
+    this.yearsOfExperience = yearsOfExperience;
+    this.name = name;
+    this.cleaningRepetoire = cleaningRepetoire;
+}
 
-
-// initializing the 
-
+// Initialise the  Object
+var houseKeeper1 = new HouseKeeper1(9, "Tom", ["lobby", "bedroom"]);
 
