577397b08656bef7c3acf1cd02d4f8445d5016f7
Ambesawi <ambesawi77@gmail.com>
2026-02-09 22:17:21 -0700

    object.js

---
+++ section_18/Drum_Kit_Starting_Files/Drum_Kit_Starting_Files/object.js
@@ -0,0 +1,13 @@
+
+// creating obeject calld houskeeper
+
+var houskeeper =
+
+
+// Constructor Function for the object. 
+// Its like creating a fuctory for the object house keeper
+
+
+// initializing the 
+
+
