b6d76a6d96dc59dd439175938e4966b0d5621725
Ambesawi <ambesawi77@gmail.com>
2026-02-25 12:34:10 -0700

    index.js

---
+++ section_23/NPM_2.3/index.js
@@ -0,0 +1,12 @@
+
+// import superheroes from "superheroes";
+
+// const name = superheroes.random();
+
+// console.log(`I am ${name}!`);
+
+import { randomSuperhero } from "superheroes";
+
+const heroName = randomSuperhero();
+
+console.log(`My superhero name is ${heroName}.`);
