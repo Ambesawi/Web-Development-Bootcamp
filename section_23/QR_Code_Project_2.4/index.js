77f24a930aa4eb89c8b85ddb43b2d5bd24675396
Ambesawi <ambesawi77@gmail.com>
2026-03-01 23:57:01 -0700

    index.js

--- section_23/QR_Code_Project_2.4/index.js
+++ section_23/QR_Code_Project_2.4/index.js
@@ -4,7 +4,8 @@
 3. Create a txt file to save the user input using the native fs node module.
 */
 import inquirer from 'inquirer';
-var qr = require('qr-image');
+import qr from 'qr-image';
+import fs from 'fs';
 
 inquirer
     .prompt([{
@@ -14,8 +15,8 @@ inquirer
     .then((answers) => {
         // Use user feedback for... whatever!!
         const url = answers.URL;
-        var qr_svg = qr.image('I love QR!');
-        qr_svg.pipe(require('fs').createWriteStream('i_love_qr.svg'));
+        var qr_svg = qr.image(url);
+        qr_svg.pipe(fs.createWriteStream('qr-img.png'));
     })
     .catch((error) => {
         if (error.isTtyError) {
